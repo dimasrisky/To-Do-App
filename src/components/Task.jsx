3be2c6b74c0adc717030298a2b8901c754cc0fd9
Dimz~ <rizkyalphazee@gmail.com>
2023-10-10 11:31:20 +0700

    Add: Build Version

---
+++ src/components/Task.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import deleteIcon from '/icon/delete.svg'
+import editIcon from '/icon/edit.svg'
+import { motion } from 'framer-motion'
+
+const Task = ({ id, task, red, green, blue, setTaskList}) => {
+  return (
+    <>
+     <motion.div className={`w-full text-white font-inter text-sm font-semibold flex justify-between items-center p-[1rem] rounded-md mb-4`} style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})`}} initial={{opacity: 0, x: -100}} animate={{opacity: 1, x: 0}} transition={{duration: 0.6, type: "spring"}} exit={{x: -100, opacity: 0}} >
+        <h1>{ task }</h1>
+        <div className='flex gap-3'>
+            <button onClick={() => alert("Maaf Fitur Ini Masih Dalam Pengembangan")}>
+                <img src={editIcon} alt="edit" width='20px' className='hover:scale-110'/>
+            </button>
+            <button onClick={() => setTaskList(prev => prev.filter(eachTask => eachTask.id != id))}>
+                <img src={deleteIcon} alt="delete" width='20px' className='hover:scale-110'/>
+            </button>
+        </div>
+    </motion.div>
+    </>
+  )
+}
+
+export default Task
